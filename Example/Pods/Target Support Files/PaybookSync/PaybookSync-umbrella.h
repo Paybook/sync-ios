89e78b7ca0634b478dfc67cafd43e63cfc28cc70
Gabriel Villarreal <gabriel@paybook.me>
2016-06-06 13:50:05 -0500

    Initial Commit

---
+++ Example/Pods/Target Support Files/PaybookSync/PaybookSync-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double PaybookSyncVersionNumber;
+FOUNDATION_EXPORT const unsigned char PaybookSyncVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-PaybookSync_Example/Pods-PaybookSync_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PaybookSync_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PaybookSync_ExampleVersionString[];
+
